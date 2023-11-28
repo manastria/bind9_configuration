5ee25ef0b66ed8249b8f05453c6ef2f77db5ea27
manastria <manastria@users.noreply.github.com>
2023-11-28 16:29:17 +0100

    AppArmor : refactoring et ajout des logs

--- config_apparmor.sh
+++ config_apparmor.sh
@@ -3,20 +3,21 @@
 # Chemin vers le fichier de configuration AppArmor pour BIND
 APPARMOR_CONF="/etc/apparmor.d/usr.sbin.named"
 
-# Lignes à ajouter
-LINE1="/etc/bind/zones/ r,"
-LINE2="/etc/bind/zones/** rw,"
+# Fonction pour ajouter une ligne dans le fichier AppArmor si elle n'existe pas déjà
+ajouter_permission() {
+    local line=$1
+    if ! grep -q "$line" "$APPARMOR_CONF"; then
+        sed -i "/^}/i $line" "$APPARMOR_CONF"
+    fi
+}
 
-# Vérifie si les lignes existent déjà
-if ! grep -q "$LINE1" "$APPARMOR_CONF"; then
-    # Insère $LINE1 avant l'accolade fermante
-    sed -i "/^}/i $LINE1" "$APPARMOR_CONF"
-fi
+# Ajout des permissions pour les zones
+ajouter_permission "/etc/bind/zones/ r,"
+ajouter_permission "/etc/bind/zones/** rw,"
 
-if ! grep -q "$LINE2" "$APPARMOR_CONF"; then
-    # Insère $LINE2 avant l'accolade fermante
-    sed -i "/^}/i $LINE2" "$APPARMOR_CONF"
-fi
+# Ajout de la permission pour les fichiers de logs
+ajouter_permission "/var/log/named/ rw,"
+ajouter_permission "/var/log/named/** rw,"
 
 # Redémarre AppArmor pour appliquer les modifications
 systemctl restart apparmor
